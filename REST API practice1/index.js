5eea32ac7dcc42aa1ba5383dd565d1f842eb0f26
Prajwal Haniya <82883854+Prajwalhn18@users.noreply.github.com>
2021-09-06 11:16:32 +0530

    Update index.js

--- REST API practice1/index.js
+++ REST API practice1/index.js
@@ -27,11 +27,6 @@ app.get('/api/courses/:id', (req,res) =>{
 });
 
 app.post('/api/courses',(req,res) => {
-
-    if(!req.body.name || req.body.name.length < 3){
-        res.status(400).send("Name is required and should be more than 3 characters");
-        return;
-    }
     const course = {
         id:courses.length+1,
         name: req.body.name
