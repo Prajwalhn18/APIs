2f37e0729143af3c2898651ada654480ec61323e
Prajwal Haniya <82883854+Prajwalhn18@users.noreply.github.com>
2021-09-06 10:53:12 +0530

    Update index.js

--- REST API practice1/index.js
+++ REST API practice1/index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const app = express();
+
+const courses = [
+    {id:1,name:'course1'},
+    {id:2,name:'course2'},
+    {id:3,name:'course3'}
+];
 app.get('/',(req,res) => {
     res.send('Hello');
 });
 
-app.get('/api/numbers',(req,res)=>{
-    var a = [1,2,3];
-    res.send(a);
+app.get('/api/courses',(req,res)=>{
+    res.send(courses);
+});
+
+app.get('/api/courses/:id', (req,res) =>{
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) {
+        res.status(404).send("Page not found");
+    }
+    else{
+        res.send(course);
+    }
 });
 
 //ENV
